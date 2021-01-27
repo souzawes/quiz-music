41004297d03b732691732112d96c9e234d697aa0
Wesley Souza <souzawes93@gmail.com>
2021-01-27 14:55:32 -0300

    Primeiro commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json'
+import Footer from '../src/components/Footer/'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget'
+
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Music</h1>
+          </Widget.Header>
+          <Widget.Content>
+            asdfasdfas
+          </Widget.Content>
+        </Widget>
+        <Widget>          
+          <Widget.Content>
+            asdfasdfas
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/souzawes"/>
+    </QuizBackground>
+  );
 }
